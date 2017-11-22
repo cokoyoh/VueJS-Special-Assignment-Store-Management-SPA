49b404550ef88c7150b031d595d77062af043c0c
cokoyoh <charlesokoyoh@gmail.com>
2017-11-22 20:32:37 +0300

    foundation integrated in the setup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Foundation from 'foundation-sites'
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App),
+    Foundation,
 })
