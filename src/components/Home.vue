c67b787242c2226053a1ea754698e86e5508bea3
cokoyoh <charlesokoyoh@gmail.com>
2017-11-22 21:17:02 +0300

    sweet alert set up successful

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,19 @@
 <template>
     <div class = "home" id = "home">
-        <div class = "button success">Trial Foundation Home</div>
+        <div class = "button success" @click="onSweetAlert">Trial Foundation Home</div>
     </div>
 </template>
 <script>
+    import swal from 'sweetalert2'
     export default {
         data() {
             return{
 
+            }
+        },
+        methods: {
+            onSweetAlert(){
+                swal('Trial','This is a success','success');
             }
         }
     }
